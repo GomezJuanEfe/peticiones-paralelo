418db333f64c237097e4c473af22d449fd1c4d85
Juan Gomez <jf.gomezramirez@gmail.com>
2023-07-23 21:55:08 -0500

    Parallel solution 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Parallel from './components/parallel'
+import Parallel2 from './components/parallel2'
 
 function App() {
 
   return (
     <>
-      <Parallel />
+      {/* <Parallel /> */}
+      <Parallel2 />
     </>
   )
 }
